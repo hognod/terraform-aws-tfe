57591fc4696aabb50a28ec27c67a9475a686bacb
hognod <dongoh312@icloud.com>
2025-11-26 17:15:28 +0900

    test output

--- 11_iam_role.tf
+++ 11_iam_role.tf
@@ -79,7 +79,7 @@ resource "aws_iam_role_policy_attachment" "eks_node_group_container_registry_rea
 # IRSA
 
 output "test" {
-  value = aws_eks_cluster.main.identity
+  value = aws_eks_cluster.main.identity.oidc.issuer
 }
 
 # data "aws_iam_policy_document" "irsa_assume_role" {
