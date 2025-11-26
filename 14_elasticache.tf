f4e3e09f9754ad89862520b9118511c6e731ad56
hognod <dongoh312@icloud.com>
2025-11-26 11:22:35 +0900

    add redis

--- 14_elasticache.tf
+++ 14_elasticache.tf
@@ -10,7 +10,7 @@ resource "aws_elasticache_cluster" "main" {
   cluster_id = "hognod-elasticache"
   engine = "redis" # memcached / redis / valkey
   node_type = var.elasticache_node_type
-  num_cache_nodes = 0
+  num_cache_nodes = 1
   subnet_group_name = aws_elasticache_subnet_group.main.name
   security_group_ids = [
     aws_security_group.ealsticache.id
