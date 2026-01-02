137c3604108efb214ba4832db006f8026419ede8
hognod <dongoh312@icloud.com>
2026-01-02 16:32:00 +0900

    temp test

--- 19_terraform_data.tf
+++ 19_terraform_data.tf
@@ -37,30 +37,20 @@ resource "terraform_data" "bastion" {
   }
 
   provisioner "remote-exec" {
+    script = "./scripts/init.sh"
+  }
+
+  provisioner "remote-exec" {
     #on_failure = continue
 
     inline = [
       # prerequisites
       "chmod 400 ~/${var.prefix}.pem",
 
-      # aws cli install
-      "curl \"https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip\" -o \"awscliv2.zip\"",
-      "sudo apt install -y unzip",
-      "unzip awscliv2.zip",
-      "sudo ./aws/install",
+      # Kube config
       "aws eks update-kubeconfig --region ${var.region} --name ${aws_eks_cluster.main.name}",
 
-      # kubectl install
-      "curl -LO \"https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl\"",
-      "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",
-      "echo 'alias k=\"kubectl\"' >> ~/.bashrc",
-
-      # helm install
-      "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4",
-      "chmod 700 get_helm.sh",
-      "./get_helm.sh",
-
-      # TFE Settings
+      # Create TFE Namespace.
       "kubectl create namespace ${var.tfe_kube_namespace}",
 
       # # Secrets
@@ -82,11 +72,6 @@ resource "terraform_data" "bastion" {
       # # TFE Agent Service Account
       # "kubectl create --namespace ${var.tfe_kube_namespace}-agents -f terraform-agent-sa.yaml",
 
-      # GitLab Installer
-      "mkdir -p ~/gitlab-installer",
-      "curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash",
-      "sudo apt-get install -y --download-only gitlab-ce=18.4.5-ce.0",
-      "sudo mv /var/cache/apt/archives/*.deb ~/gitlab-installer",
       "scp -i ~/${var.prefix}.pem -r ~/gitlab-installer ${var.instance_user}@${aws_instance.gitlab.private_ip}:"
     ]
   }
---
+++ scripts/init.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
+
+# aws cli install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install -y unzip
+unzip awscliv2.zip
+sudo ./aws/install
+
+# kubectl install
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+echo 'alias k=\"kubectl\"' >> ~/.bashrc
+
+# helm install
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
+
+# GitLab Installer
+mkdir -p ~/gitlab-installer
+curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+sudo apt-get install -y --download-only gitlab-ce=18.4.5-ce.0
+sudo mv /var/cache/apt/archives/*.deb ~/gitlab-installer
